d55146323b6b376e849a5e8e1db8a52f66e70045
robinsondbn <ieisrobinson@gmail.com>
2018-10-09 21:01:09 -0500

    Cambios de estilos y añadidas las fuentes

--- App/Controllers/LoginController.js
+++ App/Controllers/LoginController.js
@@ -1,13 +1,18 @@
-app.controller('LoginController',['$scope', 'User', function($scope, User){
+app.controller('LoginController',['$scope', '$state', 'User', function($scope, $state, User){
     
     $scope.user = User;
     
     $scope.login = () =>{
+        
         $scope.user.login();
         $scope.user.init({
             email : null,
             password : null
         });
+    }
+    $scope.redirect = () =>{
+        $('#loginOfThis').modal('hide');
+//        $state.go('registers');
     }
     
 }]);
