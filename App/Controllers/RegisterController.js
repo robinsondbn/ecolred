9e439cd4ff9decf7c2bc939fe7f3d9a50918800a
robinsondbn <ieisrobinson@gmail.com>
2018-10-17 21:24:26 -0500

    estilos de pagina de registro, sweet alert de confirmacion de envio de datos y correciones

--- App/Controllers/RegisterController.js
+++ App/Controllers/RegisterController.js
@@ -3,13 +3,30 @@ app.controller('RegisterController', ['$scope','User',function($scope,User){
     $scope.user = User;
     
     $scope.register = () => {
-        var password = $scope.user.password;
-        var confirmPassword = $scope.user.confirmpassword;
-        if(password === confirmPassword){
+      var password = $scope.user.password;
+      var confirmPassword = $scope.user.confirmpassword;
+      if(password === confirmPassword){
+        swal({
+          title: "Formulario completo",
+          text: "¿Esta seguro que desea enviar la informacion?",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            swal("La informacion a sido enviada correctamente", {
+              icon: "success",
+            });
             $scope.user.create();
-        }else{
-            swal('Las contraseñas no coinciden','Lo campos de las contraseñas deben de ser iguales','error');
-        }
+          } else {
+            swal("De acuerdo, la informacion nofue enviada");
+          }
+        });
+
+      }else{
+          swal('Las contraseñas no coinciden','Lo campos de las contraseñas deben de ser iguales','error');
+      }
     }
     
 }]);
